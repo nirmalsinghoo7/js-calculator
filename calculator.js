d29ce7ecb0ef195fe62864bb6dade30c93cc23b9
Nirmal Singh <12962402+nirmalsinghoo7@users.noreply.github.com>
2022-10-01 19:40:51 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,5 @@
-console.log('This is my project Calculator 65 from JavaScript course');
-
 let inputDigits = document.getElementById('input-digits');
-
-
 let sumTotal = document.getElementById('sumTotal');
-
-
 buttons = document.querySelectorAll('button');
 let screenValue = '';
 // Remove last character
@@ -13,12 +7,9 @@ const popLastChar = (str) => {
 	return str.slice(0, -1);
 };
 
-
 for(item of buttons) {
   item.addEventListener('click', (e)=>{
-    console.log(inputDigits.innerHTML);
     buttonText = e.target.innerText;
-    console.log('Button text is ', buttonText);
     if(buttonText == 'X'){
       buttonText = '*';
       screenValue += buttonText;
@@ -36,7 +27,6 @@ for(item of buttons) {
 
     else if(buttonText =='='){
       sumTotal.innerHTML = eval(screenValue);
-      //sumTotal = inputDigits.innerHTML;
     }
     else{
       screenValue += buttonText;
